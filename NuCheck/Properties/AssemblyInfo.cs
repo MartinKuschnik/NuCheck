7ccaef382de21eaca4fc97c20577959aac5912c0
Martin Kuschnik <martin-kuschnik@live.com>
2015-09-04 22:54:13 +0200

    * adjusted assembly properties

--- NuCheck/Properties/AssemblyInfo.cs
+++ NuCheck/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using NuCheck;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NuCheck")]
+[assembly: AssemblyTitle(AssemblyProperties.Name)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NuCheck")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct(AssemblyProperties.Name)]
+[assembly: AssemblyCopyright(AssemblyProperties.Copyright)]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(AssemblyProperties.Version)]
+[assembly: AssemblyFileVersion(AssemblyProperties.Version)]
---
+++ NuCheck/Properties/AssemblyProperties.cs
@@ -0,0 +1,9 @@
+﻿namespace NuCheck
+{
+    internal static class AssemblyProperties
+    {
+        internal const string Version = "1.0.1509.04001"; // X.X.Year_Year_Month_Mont.Day_Day_BuildOfDayBuildOfDayBuildOfDay
+        internal const string Name = "NuCheck";
+        internal const string Copyright = "© 2015 Martin Kuschnik";
+    }
+}
