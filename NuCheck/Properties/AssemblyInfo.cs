4eefd16af34cbaee95505a7cc010780518d827b1
Martin Kuschnik <martin-kuschnik@live.com>
2015-09-05 01:57:42 +0200

    * prepared assembly properties for version number patching during build

--- NuCheck/Properties/AssemblyInfo.cs
+++ NuCheck/Properties/AssemblyInfo.cs
@@ -22,15 +22,4 @@ using NuCheck;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2db7bc35-c8fe-4686-82ff-a924309843e6")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion(AssemblyProperties.Version)]
-[assembly: AssemblyFileVersion(AssemblyProperties.Version)]
+[assembly: AssemblyVersion("1.0.*")]
--- NuCheck/Properties/AssemblyProperties.cs
+++ NuCheck/Properties/AssemblyProperties.cs
@@ -2,7 +2,6 @@
 {
     internal static class AssemblyProperties
     {
-        internal const string Version = "1.0.1509.04001"; // X.X.Year_Year_Month_Mont.Day_Day_BuildOfDayBuildOfDayBuildOfDay
         internal const string Name = "NuCheck";
         internal const string Copyright = "© 2015 Martin Kuschnik";
     }
