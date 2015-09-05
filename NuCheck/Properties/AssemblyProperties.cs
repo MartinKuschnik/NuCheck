f5396cba3bca1675131f29bc3a5dc20e06e48b0d
Martin Kuschnik <martin-kuschnik@live.com>
2015-09-05 19:08:50 +0200

    * added some missing comments

--- NuCheck/Properties/AssemblyProperties.cs
+++ NuCheck/Properties/AssemblyProperties.cs
@@ -1,8 +1,18 @@
 ﻿namespace NuCheck
 {
+    /// <summary>
+    /// Static class which contains assembly properties.
+    /// </summary>
     internal static class AssemblyProperties
     {
+        /// <summary>
+        /// The name of the assembly.
+        /// </summary>
         internal const string Name = "NuCheck";
+
+        /// <summary>
+        /// The copyright string.
+        /// </summary>
         internal const string Copyright = "© 2015 Martin Kuschnik";
     }
 }
